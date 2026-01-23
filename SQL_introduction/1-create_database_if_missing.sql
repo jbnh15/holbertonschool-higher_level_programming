bb5efe5fa7984b916686c74f2ec22f04d4b7db79
root <root@ip-10-42-146-15.ec2.internal>
2026-01-23 02:43:16 +0000

    FIX 04

---
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,3 @@
+-- Create database hbtn_0c_0 if it does not exist
+-- This script creates the database hbtn_0c_0 safely
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
---
+++ SQL_introduction/README.md
@@ -0,0 +1,3 @@
+# holbertonschool-higher_level_programming
+
+SQL Introduction (MySQL 8.0) - Holberton tasks.
