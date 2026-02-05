8de46c126c4dccefd1cdc3720ee4ddf0ec3dc095
jbnh15 <jose.noguera@anahuac.mx>
2026-02-05 17:12:17 +0000

    4.0

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,7 @@
--- Show privileges
+-- Create the users if they don't exist (so SHOW GRANTS won't error)
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
+
+-- List their privileges
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
